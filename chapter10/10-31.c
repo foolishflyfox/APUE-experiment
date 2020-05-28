b16697048d40de526f3af1388b0abd5545b99711
foolishflyfox <fenghuabin1992@163.com>
2020-05-28 19:59:07 +0800

    update 2020-05-28 19:59:07

---
+++ chapter10/10-31.c
@@ -0,0 +1,38 @@
+// 10-31.c
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#define BUFFSIZE 1024
+
+static void sig_tstp(int signo){ /* signal handler for SIGTSTP */
+    sigset_t mask;
+    /* ... move cursor to lower left corner, reset tty mode ... */
+    /*
+     * Unblock SIGTSTP, since it's blocked while we're handling it.
+     */
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGTSTP);
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
+
+    signal(SIGTSTP, SIG_DFL);  /* reset disposition to default */
+    kill(getpid(), SIGTSTP);   /* and send the signal to ourself */
+    /* we won't return from the kill until we're continued */
+    signal(SIGTSTP, sig_tstp); /* resetablish signal handler */
+    /* ... reset tty mode, redraw screen ... */
+}
+int main(void){
+    int n;
+    char buf[BUFFSIZE];
+    /*
+     * Only catch SIGTSTP if we're running with a job-control shell.
+     */
+    if(signal(SIGTSTP, SIG_IGN) == SIG_DFL)
+        signal(SIGTSTP, sig_tstp);
+    while((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
+        if(write(STDOUT_FILENO, buf, n) != n){
+            perror("write error"); _exit(1);
+        }
+    if(n < 0) {
+        perror("read error"); _exit(1);
+    }       
+}
