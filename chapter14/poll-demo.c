9ef164d1db484d75bb828cd635e189cca0291030
foolishflyfox <fenghuabin1992@163.com>
2020-06-08 19:58:49 +0800

    update 2020-06-08 19:58:48

--- chapter14/poll-demo.c
+++ chapter14/poll-demo.c
@@ -29,17 +29,28 @@ int main(){
     struct pollfd pfds[2];
     pfds[0].fd = fd1;  pfds[0].events = POLLIN;
     pfds[1].fd = fd2;  pfds[1].events = POLLIN;
-    for( ; ;){
-        printf("xxx\n");
+    int cnt = 2;
+    while( cnt > 0 ){
         int res = poll(pfds, 2, 5000);// 时间以毫秒为单位
-        printf("yyy\n");
         if(res == -1){  // 出错，捕捉到一个信号
             break;
         }else  if(res == 0){  // 指定时间内没有收到信号
             break;
         }else{
+            // 与 select 不同，到达文件尾不会使 POLLIN 置位
             if(pfds[0].revents & POLLIN) ReadData(pfds[0].fd);
+            if(pfds[0].revents & POLLHUP) {  // 管道另一方已经关闭
+                close(pfds[0].fd);
+                pfds[0].fd *= -1;  // 负值的文件描述符将被忽略
+                --cnt;
+            }
             if(pfds[1].revents & POLLIN) ReadData(pfds[1].fd);
+            if(pfds[1].revents & POLLHUP){
+                close(pfds[1].fd);
+                pfds[1].fd *= -1;
+                --cnt;
+            }
         }
     }
+    printf("%ld exit\n", time(NULL));
 }
