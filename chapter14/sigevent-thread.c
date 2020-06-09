dfae2d1057e48702051809ff0c62cb35ef255a8e
foolishflyfox <fenghuabin1992@163.com>
2020-06-09 19:38:42 +0800

    update 2020-06-09 19:38:42

--- chapter14/sigevent-thread.c
+++ chapter14/sigevent-thread.c
@@ -1,17 +1,17 @@
-// sigevent-signal.c
+// sigevent-thread.c
 #include <stdio.h>
-#include <signal.h>
 #include <unistd.h>
 #include <aio.h>
 #include <string.h>
-#include <fcntl.h>
 #include <pthread.h>
+#include <fcntl.h>
 
 #define BSZ 10
 char buf[BSZ+1];
 struct aiocb cb;
-void sig_usr1(int signo){
-    printf("sig_usr1 thread: %lu\n", (unsigned long)pthread_self());
+void foo(union sigval val){
+    printf("foo thread: %lu, val = %d\n", 
+        (unsigned long)pthread_self(), val.sival_int);
     printf("aio_error(&cb) = %d\n", aio_error(&cb));
     printf("aio_return(&cb) = %zd\n", aio_return(&cb));
     printf("read %zd chars: %s\n", strlen(buf), buf);
@@ -24,15 +24,15 @@ int main(int argc, char *argv[]){
     }
     cb.aio_offset = 0;
     cb.aio_nbytes = BSZ;
-    // 完成读取后，发送信号 SIGUSR1
-    cb.aio_sigevent.sigev_notify = SIGEV_SIGNAL;
-    cb.aio_sigevent.sigev_signo = SIGUSR1;
-    if(signal(SIGUSR1, sig_usr1) == SIG_ERR){
-        perror("signal(SIGUSR1) error"); return 1;
-    }
+    // 完成读取后，
+    cb.aio_sigevent.sigev_notify = SIGEV_THREAD;
+    // cb.aio_sigevent.sigev_signo = SIGUSR1;
+    cb.aio_sigevent.sigev_notify_function = foo;
+    cb.aio_sigevent.sigev_notify_attributes = NULL;
+    cb.aio_sigevent.sigev_value.sival_int = 11;
+    printf("main thread: %lu\n", (unsigned long)pthread_self());
     if(aio_read(&cb) == -1){
         perror("aio_read error"); return 1;
     }
-    printf("main thread: %lu\n", (unsigned long)pthread_self());
     while(1) pause();
 }
